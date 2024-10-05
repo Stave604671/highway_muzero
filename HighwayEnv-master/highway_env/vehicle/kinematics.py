2fbb733ea73c996bc70d5d8a7a4e5d6a52e44a82
Stave604671 <604671440@qq.com>
2024-10-05 08:53:14 +0800

    换道后成功换道至中心点

--- HighwayEnv-master/highway_env/vehicle/kinematics.py
+++ HighwayEnv-master/highway_env/vehicle/kinematics.py
@@ -4,6 +4,7 @@ import copy
 from collections import deque
 
 import numpy as np
+# import ray
 from ray import logger
 from highway_env.road.road import Road
 from highway_env.utils import Vector
@@ -217,14 +218,27 @@ class Vehicle(RoadObject):
 
         # 处理换道逻辑
         new_lane_index = self.road.network.get_closest_lane_index(self.position, self.heading)
+        if self.is_observed:
+            logger.info(f"当前车道：{self.lane_index[2]}。当前位置：{self.position[1]} 换道目标车道：{new_lane_index[2]}.")
         if new_lane_index[2] != self.lane_index[2]:
             # 计算新车道中心位置，假设车道宽度为4
-            target_lane_center_y = (new_lane_index[2] + 0.5) * 4  # 车道宽度为4
+            """
+            0
+            --1     1车道中心坐标=（0+0.5）*4=0.5*4=2
+            4
+            --2     2车道中心坐标=（1+0.5）*4=1.5*4=6
+            8
+            --3     3车道中心坐标=（2+0.5）*4=2.5*4-2=10-2 = 8
+            12
+            --4     4车道中心坐标=（3+0.5）*4-2=3.5*4-2=14-2=12
+            16
+            """
+            target_lane_center_y = (new_lane_index[2] + 0.5) * 4 - 2  # 车道宽度为4
             self.position[1] = target_lane_center_y  # 移动车辆到新车道的中心位置
             self.lane_index = new_lane_index
             self.lane = self.road.network.get_lane(self.lane_index)
             self.heading = self.lane.heading_at(self.position[0])  # 将航向调整为车道的方向
-
+            logger.info(f"{self.position[1]}--{target_lane_center_y}--{self.lane_index}")
         # 更新速度
         self.speed += self.action["acceleration"] * dt
 
