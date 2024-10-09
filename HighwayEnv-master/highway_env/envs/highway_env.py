657b618603300258e4b6b124e138f4d8f9aeddee
Stave604671 <604671440@qq.com>
2024-10-10 07:15:28 +0800

    删除重要性权重

--- HighwayEnv-master/highway_env/envs/highway_env.py
+++ HighwayEnv-master/highway_env/envs/highway_env.py
@@ -103,7 +103,7 @@ class HighwayEnv(AbstractEnv):
         """
         rewards = self._rewards(action)
         reward = sum(
-            reward for name, reward in rewards.items()
+            self.config.get(name, 0) * reward for name, reward in rewards.items()
         )
         if self.config["normalize_reward"]:
             reward = utils.lmap(
