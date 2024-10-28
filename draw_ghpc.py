1a4344cc31598ca1fbf95ed8262ba8cbee597898
Stave604671 <604671440@qq.com>
2024-10-28 23:44:23 +0800

    绘制规划偏差图

--- draw_ghpc.py
+++ draw_ghpc.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import pickle
 
@@ -8,6 +9,7 @@ from tensorboard.backend.event_processing import event_accumulator
 import matplotlib.ticker as ticker  # 用于控制刻度格式
 from matplotlib import font_manager
 from matplotlib.lines import Line2D
+from scipy.interpolate import splrep, splev
 from scipy.interpolate import CubicSpline
 
 
@@ -78,37 +80,19 @@ def get_observed_vehicle_road_change(game_history):
     return state_collect
 
 
-def draw_plot_on_ax(ax, tag_data, choice_flag):
-    colors = ['blue', 'green', 'red', 'orange', 'purple']  # 定义一些颜色
-    for idx_, (vehicle_index, vehicle_position_list) in enumerate(tag_data.items()):
-        color = colors[idx_ % len(colors)]  # 循环使用颜色
-        x_vals = range(0, len(vehicle_position_list[choice_flag]))  # x轴是时间
-        y_vals = vehicle_position_list[choice_flag]
-        cs = CubicSpline(x_vals, y_vals)  # 样条插值
-        x_interp = np.linspace(0, len(x_vals)-1, 500)  # 更密集的x轴点
-        y_interp = cs(x_interp)  # 使用样条生成平滑的y值
-        # 绘制线条
-        ax.plot(x_interp, y_interp, label=vehicle_index, color=color, linewidth=1)
-
-
-def draw_verb_jerk_vy_table(tag_data, xlabel='Time(s)', ylabel="X-speed(m/s)", choice_flag='vx'):
+def draw_ghpc_table(tag_data, choice_flag='pct', xlabel="X(m)", ylabel="Y(m)"):
     fig, ax = plt.subplots(figsize=(10, 5))  # 导出是1000*500像素的效果
-    draw_plot_on_ax(ax, tag_data, choice_flag)
+    if choice_flag == "ghpct":
+        draw_ghjg_data_on_ax1(ax, tag_data)
+    else:
+        draw_ghjg_data_pc_on_ax1(ax, tag_data, choice_flag)
     # 设置x轴刻度
     x_min, x_max = ax.get_xlim()
-    xticks = np.linspace(x_min, x_max, 6)  # 生成 5 等份的刻度线
+    xticks = np.linspace(x_min, x_max, 10)  # 生成 5 等份的刻度线
     xlabels = [f'{round(x):1.1f}' for x in xticks]
     xlabels[0] = "0.0"
-    # 设置y轴刻度
-    if "v" in choice_flag:
-        y_min, y_max = ax.get_ylim()
-        y_ticks = np.arange(y_min, y_max, (y_max-y_min)/10)
-        # y_ticks = np.arange(-30, 30, 2)
-    elif "jerk" in choice_flag:
-        y_min, y_max = ax.get_ylim()
-        y_ticks = np.arange(y_min, y_max, (y_max-y_min)/10)
-    else:
-        y_ticks = np.arange(0, 100, 10)
+    y_min, y_max = ax.get_ylim()
+    y_ticks = np.linspace(y_min, y_max, 5)
     ylabels = [f'{x:.2f}' for x in y_ticks]
     setup_plot_style(ax, xlabel=xlabel, ylabel=ylabel, x_ticks=xticks, xtick_labels=xlabels,
                      y_ticks=y_ticks, ytick_labels=ylabels)
@@ -122,26 +106,105 @@ def draw_verb_jerk_vy_table(tag_data, xlabel='Time(s)', ylabel="X-speed(m/s)", c
     # 显示图表
 
 
+
+
+
 def get_vehicle_state_in_game_history(game_history, time_range, vehicle_tag, vehicle_collect_dict):
     for vehicles in game_history.vehicle_history[time_range[0]: time_range[1]]:
         vehicle = vehicles[0]
         vehicle_id = vehicle_tag
+
         if not vehicle_collect_dict.get(vehicle_id):
-            vehicle_collect_dict[vehicle_id] = {"x": [vehicle.position[0]],
-                                                "y": [vehicle.position[1]],
+            vehicle_collect_dict[vehicle_id] = {"x": [vehicle.position_recent[0],
+                                                      vehicle.position[0]],
+                                                "y": [vehicle.position_recent[1],
+                                                      vehicle.position[1]],
+                                                "new_x": [vehicle.position_recent[0],
+                                                          vehicle.new_position[0]],
+                                                "new_y": [vehicle.position_recent[1],
+                                                          vehicle.new_position[1]],
                                                 "vx": [vehicle.get_verb_x],
                                                 "vy": [vehicle.get_verb_y],
                                                 "jerk_x": [vehicle.get_jerk_x],
-                                                "jerk_y": [vehicle.get_jerk_y]}
+                                                "jerk_y": [vehicle.get_jerk_y],
+                                                "lateral_deviation": [0, vehicle.position[0] - vehicle.new_position[0]],
+                                                # 横向偏差
+                                                "longitudinal_deviation": [0,
+                                                    vehicle.position[1] - vehicle.new_position[1]]  # 纵向偏差
+                                                }
 
         else:
             vehicle_collect_dict[vehicle_id]['x'].append(vehicle.position[0])
             vehicle_collect_dict[vehicle_id]['y'].append(vehicle.position[1])
+            vehicle_collect_dict[vehicle_id]['new_x'].append(vehicle.new_position[0])
+            vehicle_collect_dict[vehicle_id]['new_y'].append(vehicle.new_position[1])
             vehicle_collect_dict[vehicle_id]['vx'].append(vehicle.get_verb_x)
             vehicle_collect_dict[vehicle_id]['vy'].append(vehicle.get_verb_y)
             vehicle_collect_dict[vehicle_id]['jerk_x'].append(vehicle.get_jerk_x)
             vehicle_collect_dict[vehicle_id]['jerk_y'].append(vehicle.get_jerk_y)
+            vehicle_collect_dict[vehicle_id]['lateral_deviation'].append(vehicle.position[0] - vehicle.new_position[0])
+            vehicle_collect_dict[vehicle_id]['longitudinal_deviation'].append(
+                vehicle.position[1] - vehicle.new_position[1])
+
     return vehicle_collect_dict
+
+
+def draw_ghjg_data_on_ax1(ax, tag_data):
+    """
+    读取观测空间数据绘制规划结果
+    :param ax:
+    :param tag_data:
+    :return:
+    """
+    colors = ['blue', 'green', 'red', 'orange', 'purple']  # 定义一些颜色
+
+    for idx_, (vehicle_index, vehicle_position_list) in enumerate(tag_data.items()):
+        color = colors[idx_ % len(colors)]  # 循环使用颜色
+        color1 = colors[(idx_ + 1) % len(colors)]
+
+        # 控制路径
+        x_vals = vehicle_position_list['x']
+        y_vals = vehicle_position_list['y']
+
+        # 使用样条插值
+        tck = splrep(x_vals, y_vals, s=0)  # 生成样条对象，s=0 表示没有平滑调整
+        x_smooth = np.linspace(min(x_vals), max(x_vals), 200)  # 生成更密集的x坐标
+        y_smooth = splev(x_smooth, tck)  # 计算平滑后的y坐标
+
+        ax.plot(x_smooth, y_smooth, label="控制路径", color=color, linewidth=1)
+
+        # 规划路径
+        x_vals1 = vehicle_position_list['new_x']
+        y_vals1 = vehicle_position_list['new_y']
+
+        # 使用样条插值
+        tck1 = splrep(x_vals1, y_vals1, s=0)
+        x_smooth1 = np.linspace(min(x_vals1), max(x_vals1), 200)
+        y_smooth1 = splev(x_smooth1, tck1)
+
+        ax.plot(x_smooth1, y_smooth1, label="规划路径", color=color1, linewidth=1)
+
+
+def draw_ghjg_data_pc_on_ax1(ax, tag_data, choice_flag):
+    """
+    读取观测空间数据绘制规划结果
+    :param choice_flag:
+    :param ax:
+    :param tag_data:
+    :return:
+    """
+    colors = ['blue', 'green', 'red', 'orange', 'purple']  # 定义一些颜色
+
+    for idx_, (vehicle_index, vehicle_position_list) in enumerate(tag_data.items()):
+        color = colors[idx_ % len(colors)]  # 循环使用颜色
+        x_vals = range(0, len(vehicle_position_list[choice_flag]))  # x轴是时间
+
+        y_vals = vehicle_position_list[choice_flag]
+        label = choice_flag
+        cs = CubicSpline(x_vals, y_vals)  # 样条插值
+        x_interp = np.linspace(0, len(x_vals)-1, 500)  # 更密集的x轴点
+        y_interp = cs(x_interp)  # 使用样条生成平滑的y值
+        ax.plot(x_interp, y_interp, label=label, color=color, linewidth=1)
 
 
 if __name__ == '__main__':
@@ -155,10 +218,10 @@ if __name__ == '__main__':
         error_time_range = get_observed_vehicle_road_change(game_history_)
         choice_time_range = [error_time_range[1]-10, error_time_range[1]+10]
         get_vehicle_state_in_game_history(game_history_, choice_time_range, legend_name, vehicle_collect_dict)
-    draw_verb_jerk_vy_table(vehicle_collect_dict, ylabel="X-speed(m/s)", choice_flag="vx")
-    draw_verb_jerk_vy_table(vehicle_collect_dict, ylabel="X-Jerk(m/s^3)", choice_flag="jerk_x")
-    draw_verb_jerk_vy_table(vehicle_collect_dict, ylabel="Y-speed(m/s)", choice_flag="vy")
-    draw_verb_jerk_vy_table(vehicle_collect_dict, ylabel="Y-Jerk(m/s^3)", choice_flag="jerk_y")
+
+    draw_ghpc_table(vehicle_collect_dict, choice_flag="ghpct")
+    draw_ghpc_table(vehicle_collect_dict, choice_flag="lateral_deviation", xlabel="T(s)", ylabel="XError")
+    draw_ghpc_table(vehicle_collect_dict, choice_flag="longitudinal_deviation", xlabel="T(s)", ylabel="YError")
 
         # get_vehicle_state_in_game_history(game_history_, )
 
